961d2f98735954da173decf98a8b9f0b1789e4bd
满月 <z1013045921@gmail.com>
2020-02-10 23:15:31 +0800

    增加了search跳转

--- js/main.js
+++
@@ -1,6 +0,0 @@
-// function mouse_over(){
-//     document.getElementById("bg").style.backgroundColor =  '#888';
-// }
-// function mouse_out(){
-//     document.getElementById("bg").style.backgroundColor =  'rgba(0,0,0,0)';
-// }
